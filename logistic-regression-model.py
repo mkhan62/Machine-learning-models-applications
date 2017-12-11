643a0b88cf9d6604ddc61837f8fdac0560d254bd
mkhan62 <mkhan62@hawk.iit.edu>
2017-12-10 21:11:05 -0600

    logistic regression model to predict ad clicking

---
+++ logistic-regression-model.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from  sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+
+ad_data = pd.read_csv('advertising.csv')
+sns.distplot(ad_data['Age'])
+print(ad_data.columns)
+plt.title('Distribution of Age')
+plt.show()
+
+sns.pairplot(ad_data, hue='Clicked on Ad')
+plt.show()
+
+logmodel = LogisticRegression()
+X = ad_data[['Daily Time Spent on Site', 'Age', 'Area Income', 'Daily Internet Usage']]
+y = ad_data['Clicked on Ad']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+logmodel.fit(X_train, y_train)
+predictions = logmodel.predict(X_test)
+
+print(classification_report(y_test, predictions))
